b1bf955fa8eac199bc07e1b802de1430a6cc66e8
BROGGINI ANTON <anton.brggn@localhost.localdomain>
2025-04-09 10:26:48 +0200

    ajout liste

--- main_ProjetThonny.py
+++ main_ProjetThonny.py
@@ -27,6 +27,11 @@ V1 = True
 V2 = True
 # Variable globale
 Init = True
+lc = [1;2;3;4;5;6;7;8;9;10;11;12;13;14;15;16;17] #liste colone
+ll = [1;2;3;4;5;6;7;8;9;10;11;12;13;14;15;16;17;18;19;20;21;22;23] #liste ligne
+pc = 15 #position colone
+pl = 21 #position ligne
+d = L # direction (ligne (L) / colone (C))
 
 def followLine(speed:int, speed_slow:int):
     ''' Fonction qui teste si on est sur la bande noire et commande les moteurs droite et gauche.

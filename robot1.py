076587d1b51504abebadfbcd81aa2ded3c0f8af0
YannMet <hasuyaku@gmail.com>
2025-05-20 21:53:07 +0200

    musique final countdown

--- robot1.py
+++ robot1.py
@@ -26,7 +26,7 @@ MOTOR_FORWARD = 0   #avant
 MOTOR_BACKWARD = 1  #arrière
 BLACK = 1   #détection line_sensor noir
 WHITE = 0   #détection line_sensor blanc
-
+music.set_tempo(ticks=4, bpm = 124) #4 temps par mesure, 124 battements par minute
 #Instruction
 led_rgb(rgb(255,255,255))   #lumière blanche pour mieux détecter les lignes
 
@@ -54,6 +54,12 @@ def tourne_g():   #fonction pour tourner à gauche
     sleep(220)
     motor_stop(Motor.ALL)
 
+#Musique
+FINAL_COUNTDOWN = ["F#2:1", "C#3:1", "F#3:4", "C#5:1", "B4:1", "C#5:4", "F#4:4", "D2:1", "A2:1", "D3:4", "D5:1", "C#5:1", "D5:2", "C#5:2", "B4:4", "B1:1", "F#2:1", "B2:4", "D5:1", "C#5:1", "D5:4", "F#4:4",
+                   "E2:1", "B2:1", "G#3:4", "B4:1", "A4:1", "B4:2", "A4:2", "G#4:2", "B4:2", "A4:2", "F#2:1", "C#3:1", "F#3:2", "C#5:1", "B4:1", "C#5:4", "F#4:4", "D2:1", "A2:1", "D3:4", "D5:1", "C#5:1", "D5:2",
+                   "C#5:2", "B4:4", "B1:1", "F#2:1", "B2:4", "D5:1", "C#5:1", "D5:4", "F#4:4",
+                   "E2:1", "B2:1", "G#3:4", "B4:1", "A4:1", "B4:2", "A4:2", "G#4:2", "B4:2", "A4:1", "A3:1", "C#4:1", "E4:1", "A3:1", "E3:1", "G#4:1", "A4:1", "B4:1", "B2:1", "E3:1", "G#3:1", "B3:1", "G#3:1", "A4:1", "B4:1",
+                   "C#5:2", "B4:2", "A4:2", "G#4:2", "F#4:1", "F#3:1", "F#4:1", "F#3:1", "D5:1", "F#4:1", "D5:1", "F#4:1", "C#5:1", "G#4:1", "F4:1", "C#4:1", "G#3:1", "F3:1", "C#3:1", "G#2:1", "F2:1", "C#2:2"]
 #Execution du programme
 while True:
     while not Mur_Trouve: # pour sortir de la boucle à la fin du labyrinthe
@@ -78,3 +84,4 @@ while True:
                 print("fin")
                 motor_stop(Motor.ALL)
                 led_rgb(rgb(0,128,0))
+        music.play(FINAL_COUNTDOWN)
